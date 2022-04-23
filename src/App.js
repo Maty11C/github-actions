41db56ffccca33fc8432c329f85c2faebf783981
Matías Cabrera <mcabrera@cys.com.ar>
2022-04-22 23:46:58 -0300

    Edit homepage

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          App de prueba para github actions
         </p>
         <a
           className="App-link"
